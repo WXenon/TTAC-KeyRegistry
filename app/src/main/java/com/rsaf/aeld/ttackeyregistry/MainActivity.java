893bfb7ff9e12a154bfd1db51aff1c101b90e9a0
WXenon <ng_weixiong@yahoo.com>
2019-06-04 17:18:09 +0800

    .

--- app/src/main/java/com/rsaf/aeld/ttackeyregistry/MainActivity.java
+++ app/src/main/java/com/rsaf/aeld/ttackeyregistry/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rsaf.aeld.ttackeyregistry;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,68 +14,152 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    ListView keyList;
+    ListView keyList = findViewById(R.id.keycard_listView);
     String action;
     private CardArrayAdapter cardArrayAdapter;
+    RelativeLayout mainLayout = findViewById(R.id.mainScreen);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    FloatingActionButton fab = findViewById(R.id.fab);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Key Movement");
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        action = getIntent().getStringExtra("action");
+        if (action.equals("home")){
+            mainLayout.setVisibility(View.VISIBLE);
+            toolbar.setTitle("Key Movement");
+            setSupportActionBar(toolbar);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+            navigationView.setNavigationItemSelectedListener(this);
 
-        keyList = findViewById(R.id.keycard_listView);
-        cardArrayAdapter = new CardArrayAdapter(getApplicationContext(), R.layout.keycard);
+            cardArrayAdapter = new CardArrayAdapter(getApplicationContext(), R.layout.keycard);
 
-        for (int i = 0; i < 6; i++) {
-            if (i == 0){
-                Card card = new Card(String.format("%02d", i+1), String.valueOf(i+3));
-                cardArrayAdapter.add(card);
+            for (int i = 0; i < 6; i++) {
+                if (i == 0){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i+3));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 1){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i+3));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 2){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 3){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i+2));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 4){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i-3));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 5){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i-4));
+                    cardArrayAdapter.add(card);
+                }
             }
-            else if (i == 1){
-                Card card = new Card(String.format("%02d", i+1), String.valueOf(i+3));
-                cardArrayAdapter.add(card);
-            }
-            else if (i == 2){
-                Card card = new Card(String.format("%02d", i+1), String.valueOf(i));
-                cardArrayAdapter.add(card);
-            }
-            else if (i == 3){
-                Card card = new Card(String.format("%02d", i+1), String.valueOf(i+2));
-                cardArrayAdapter.add(card);
-            }
-            else if (i == 4){
-                Card card = new Card(String.format("%02d", i+1), String.valueOf(i-3));
-                cardArrayAdapter.add(card);
-            }
-            else if (i == 5){
-                Card card = new Card(String.format("%02d", i+1), String.valueOf(i-4));
-                cardArrayAdapter.add(card);
-            }
+            keyList.setAdapter(cardArrayAdapter);
+
+            keyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    if (position == 0){
+                        Intent toKeyNo = new Intent(MainActivity.this, MainActivity.class);
+                        toKeyNo.putExtra("action", "viewKey");
+                        toKeyNo.putExtra("keyNo", String.valueOf(position + 1));
+                        startActivity(toKeyNo);
+                    }
+                    else if (position == 1){
+                        Intent toKeyNo = new Intent(MainActivity.this, MainActivity.class);
+                        toKeyNo.putExtra("action", "viewKey");
+                        toKeyNo.putExtra("keyNo", String.valueOf(position + 1));
+                        startActivity(toKeyNo);
+                    }
+                    else if (position == 2){
+                        Intent toKeyNo = new Intent(MainActivity.this, MainActivity.class);
+                        toKeyNo.putExtra("action", "viewKey");
+                        toKeyNo.putExtra("keyNo", String.valueOf(position + 1));
+                        startActivity(toKeyNo);
+                    }
+                    else if (position == 3){
+                        Intent toKeyNo = new Intent(MainActivity.this, MainActivity.class);
+                        toKeyNo.putExtra("action", "viewKey");
+                        toKeyNo.putExtra("keyNo", String.valueOf(position + 1));
+                        startActivity(toKeyNo);
+                    }
+                    else if (position == 4){
+                        Intent toKeyNo = new Intent(MainActivity.this, MainActivity.class);
+                        toKeyNo.putExtra("action", "viewKey");
+                        toKeyNo.putExtra("keyNo", String.valueOf(position + 1));
+                        startActivity(toKeyNo);
+                    }
+                    else if (position == 5){
+                        Intent toKeyNo = new Intent(MainActivity.this, MainActivity.class);
+                        toKeyNo.putExtra("action", "viewKey");
+                        toKeyNo.putExtra("keyNo", String.valueOf(position + 1));
+                        startActivity(toKeyNo);
+                    }
+                }
+            });
         }
-        keyList.setAdapter(cardArrayAdapter);
+        else if (action.equals("viewKey")){
+            int keyNo = Integer.parseInt(getIntent().getStringExtra("keyNo"));
+
+
+        } else if (action.equals("viewAvailable")) {
+
+            cardArrayAdapter = new CardArrayAdapter(getApplicationContext(), R.layout.keycard);
+
+            for (int i = 0; i < 6; i++) {
+                if (i == 0){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i+3));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 1){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i+3));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 2){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 3){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i+2));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 4){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i-3));
+                    cardArrayAdapter.add(card);
+                }
+                else if (i == 5){
+                    Card card = new Card(String.format("%02d", i+1), String.valueOf(i-4));
+                    cardArrayAdapter.add(card);
+                }
+            }
+            keyList.setAdapter(cardArrayAdapter);
+        }
     }
 
     @Override
@@ -115,17 +200,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_main) {
-            // Handle the camera action
-        } else if (id == R.id.nav_available_keys) {
-
-        } else if (id == R.id.nav_sign_out) {
-
-        }
-
         if (item.isChecked()){
             DrawerLayout drawer = findViewById(R.id.drawer_layout);
             drawer.closeDrawer(GravityCompat.START);
+        }
+
+        if (id == R.id.nav_main) {
+            Intent toHome = new Intent (MainActivity.this, MainActivity.class);
+            toHome.putExtra("action", "home");
+            startActivity(toHome);
+        } else if (id == R.id.nav_available_keys) {
+            Intent toViewAvailable = new Intent(MainActivity.this, MainActivity.class);
+            toViewAvailable.putExtra("action", "viewAvailable");
+            startActivity(toViewAvailable);
+        } else if (id == R.id.nav_sign_out) {
+
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
